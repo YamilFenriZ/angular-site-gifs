1608434fdab7c3c080ebefee516a4ad8409a81ae
Yhamil FenrZ <yamilfenriz@gmail.com>
2023-07-30 20:46:37 -0400

    Finalizado sitio de Gifs

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,36 @@
+
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar imagenes:</h5>
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Buscar gifs..."
+      (keyup.enter)="searchTag()"
+      #txtTagInput
+    >
+  `
+})
+
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')// Solo Uno 1
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor(
+    private gifsService: GifsService
+  ) { }
+
+  searchTag(){
+    const newTag = this.tagInput.nativeElement.value;
+    this.gifsService.searchTag( newTag );
+    this.tagInput.nativeElement.value = '';
+  }
+
+
+}
